a2d112a2ab4dc3859f6dcece243382789acbdf0a
Maksud <80256627+mcss55@users.noreply.github.com>
2022-06-09 08:52:54 +0300

    Add files via upload

---
+++ InterFaceNumune/CV.java
@@ -0,0 +1,7 @@
+package interfaceexample;
+
+public interface CV {
+    public void dilSeviyyesi(String dil,int derece);
+    public void tecrube(int il);
+    public void proqDili(String proqDili);
+}
---
+++ InterFaceNumune/Person.java
@@ -0,0 +1,19 @@
+package interfaceexample;
+
+public class Person implements CV {
+    @Override
+    public void dilSeviyyesi(String dil, int derece) {
+
+    }
+
+    @Override
+    public void tecrube(int il) {
+
+    }
+
+    @Override
+    public void proqDili(String proqDili) {
+
+    }
+
+}
