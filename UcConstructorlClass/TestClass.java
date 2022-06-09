62142b85789b8d8c4087fd613f41f0ed6c942206
Maksud <80256627+mcss55@users.noreply.github.com>
2022-06-09 08:47:25 +0300

    Add files via upload

---
+++ UcConstructorlClass/TestClass.java
@@ -0,0 +1,9 @@
+package SturBack;
+
+public class TestClass {
+    public static void main(String[] args) {
+        StarBucks starBucks1=new StarBucks("machiato");
+        StarBucks starBucks2=new StarBucks("latte","karamel");
+        StarBucks starBucks3=new StarBucks("turkish coffe","middle sugar","little");
+    }
+}
---
+++ UcConstructorlClass/UcConstructorluClass.java
@@ -0,0 +1,20 @@
+package SturBack;
+
+public class StarBucks {
+    public String kofe;
+    public String kofeninNovu;
+    public String olchu;
+
+    public StarBucks(String kofe) {
+        this.kofe = kofe;
+    }
+    public StarBucks(String kofe, String kofeninNovu) {
+        this.kofe = kofe;
+        this.kofeninNovu = kofeninNovu;
+    }
+    public StarBucks(String kofe, String kofeninNovu, String olchu) {
+        this.kofe = kofe;
+        this.kofeninNovu = kofeninNovu;
+        this.olchu = olchu;
+    }
+}
